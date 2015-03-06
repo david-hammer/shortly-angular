87e08df37381fa79925b110c5c8176328f7c8ab5
david-hammer <dhammer126@gmail.com>
2015-03-06 15:20:59 -0800

    initial work on Links Controller and fix bcrypt function call

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,16 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, $http, Links) {
+  $scope.data = {};
+  $scope.getLinks = function(){
+    var resPromise = $http.get('/api/links');
+    resPromise.success(function(data){
+      $scope.data.links = data;
+    });
+    resPromise.error(function(data){
+      console.log('Error:', data);
+    });
+    // $scope.data.links = [{},{},{}];
+  };
+  $scope.getLinks();
 });
