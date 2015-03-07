af46a6037f4550c84229c14cd5578cce63810f58
david-hammer <dhammer126@gmail.com>
2015-03-06 16:08:22 -0800

    Refactor LinksController to use factory

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,13 +3,20 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, $http, Links) {
   $scope.data = {};
   $scope.getLinks = function(){
-    var resPromise = $http.get('/api/links');
-    resPromise.success(function(data){
-      $scope.data.links = data;
-    });
-    resPromise.error(function(data){
-      console.log('Error:', data);
-    });
+    Links.getLinks()
+      .then(function(links){
+        $scope.data.links = links;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    // var resPromise = $http.get('/api/links');
+    // resPromise.success(function(data){
+    //   $scope.data.links = data;
+    // });
+    // resPromise.error(function(data){
+    //   console.log('Error:', data);
+    // });
   };
   $scope.getLinks();
 });
